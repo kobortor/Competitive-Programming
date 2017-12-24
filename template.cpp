2152d0456c3b998bbb6e04a11f70480a9cfc438a
Joey Yu <kobortor@gmail.com>
2017-12-24 11:41:40 -0500

    Added my template file

---
+++ template.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+
+
+int main(){
+    cin.tie(0);
+    cin.sync_with_stdio(0);
+
+
+}
