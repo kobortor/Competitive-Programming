87e8aeb2a0383c254e8503648ee9d8834fa088f2
kobortor <kobortor@gmail.com>
2018-06-27 19:22:18 -0400

    Memory optimized usaco/2012/jan/s2

--- usaco/2012/jan/s2.cpp
+++ usaco/2012/jan/s2.cpp
@@ -9,37 +9,39 @@ typedef pair<int, int> pii;
 typedef pair<ll, ll> pll;
 
 const int MAXN = 20;
-const int MAXS = 105;
+const int MAXS = 101;
 
 int N;
 
-bitset<MAXN * MAXS> dp[MAXN + 1][MAXN * MAXS];
+bitset<MAXN * MAXS> dp[MAXN * MAXS];
 
 int main(){
     cin.tie(0);
     cin.sync_with_stdio(0);
 
-    dp[0][0][0] = true;
+    dp[0][0] = true;
 
     int tot = 0;
 
     cin >> N;
-    for(int a = 0; a < N; a++){
+    for(int a = 1; a <= N; a++){
         int val;
         cin >> val;
         tot += val;
 
-        for(int b = 0; b <= a * MAXS; b++){
-            dp[a + 1][b] |= dp[a][b];
-            dp[a + 1][b] |= dp[a][b] << val;
-            dp[a + 1][b + val] |= dp[a][b];
+        for(int b = a * MAXS - 1; b >= 0; b--){
+            dp[b] |= dp[b];
+            dp[b] |= dp[b] << val;
+            if(b - val >= 0){
+                dp[b] |= dp[b - val];
+            }
         }
     }
 
-    int ans = 1e9;
+    int ans = 0x3f3f3f3f;
     for(int a = 0; a <= N * MAXS; a++){
         for(int b = 0; b <= N * MAXS; b++){
-            if(dp[N][a][b]){
+            if(dp[a][b]){
                 ans = min(ans, max(max(a, b), tot - a - b));
             }
         }
