a8023789af60fe85ff7f3fb417f2f9be04723c4a
Joey Yu <kobortor@gmail.com>
2018-08-24 14:39:09 -0400

    Made cf/1029/C simpler

--- cf/1029/C.cpp
+++ cf/1029/C.cpp
@@ -8,62 +8,29 @@ typedef long long ll;
 typedef pair<int, int> pii;
 typedef pair<ll, ll> pll;
 
-const int MAXN = 2e6 + 6;
+const int MAXN = 3e5 + 5;
 
 int N;
-vector<int> uniq;
-int add[MAXN], sub[MAXN];
-int N0[MAXN], N1[MAXN];
 pii seg[MAXN];
+pii inter[MAXN];
 
 int main(){
     cin.tie(0);
     cin.sync_with_stdio(0);
 
     cin >> N;
+
+    inter[0] = pii(-1, 2e9);
     for(int a = 1; a <= N; a++){
         cin >> seg[a].first >> seg[a].second;
-        uniq.push_back(seg[a].first - 1);
-        uniq.push_back(seg[a].first);
-        uniq.push_back(seg[a].first + 1);
-
-        uniq.push_back(seg[a].second - 1);
-        uniq.push_back(seg[a].second);
-        uniq.push_back(seg[a].second + 1);
-    }
-
-    uniq.push_back(-1);
-    uniq.push_back(2e9);
-    sort(allof(uniq));
-    uniq.erase(unique(allof(uniq)), uniq.end());
-
-    for(int a = 1; a <= N; a++){
-        seg[a].first = lower_bound(allof(uniq), seg[a].first) - uniq.begin();
-        seg[a].second = lower_bound(allof(uniq), seg[a].second) - uniq.begin();
-        add[seg[a].first]++;
-        sub[seg[a].second]++;
-    }
-
-    for(int a = 1, cur = 0; a < (int)uniq.size(); a++){
-        N0[a] = N0[a - 1];
-        N1[a] = N1[a - 1];
-        if(cur == N - 1){
-            N1[a] += uniq[a] - uniq[a - 1];
-        } else if (cur == N){
-            N0[a] += uniq[a] - uniq[a - 1];
-        }
-
-        cur += add[a];
-        cur -= sub[a];
+        inter[a] = pii(max(inter[a - 1].first, seg[a].first), min(inter[a - 1].second, seg[a].second));
     }
 
     int ans = 0;
-    for(int a = 1; a <= N; a++){
-        int l = seg[a].first;
-        int r = seg[a].second;
-
-        ans = max(ans, N1[l] + (N1[(int)uniq.size() - 1] - N1[r]) + (N0[r] - N0[l]));
+    for(int a = N, L = -1, R = 2e9; a >= 1; a--){
+        ans = max(ans, min(R, inter[a - 1].second) - max(L, inter[a - 1].first));
+        L = max(L, seg[a].first);
+        R = min(R, seg[a].second);
     }
-
     cout << ans;
 }
