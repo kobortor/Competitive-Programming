ed1bcd941ffd26ec63ddb0562b924c67f874cfea
3f9754c9ff87dc4cb66061b1582c56272e8ec83d <kobortor@gmail.com>
2019-07-27 14:04:21 -0400

    Added the mkcf script

---
+++ scripts/mkcf.sh
@@ -0,0 +1,31 @@
+function mkcf () {
+    if (( $# != 2 )); then
+        echo We need exactly 2 arguments!
+        return
+    fi
+
+    contest_id=$1
+    problem_id=$2
+
+    if (( ${#contest_id} > 4 )); then
+        echo Contest length must be 4 characters or less
+        return
+    else
+        contest_id=$(printf %04d $contest_id)
+    fi
+
+    folder_name=$(basename $(pwd))
+    if [ "$folder_name" = "Competitive-Programming" ]; then
+        echo Setting targ=cf/$contest_id/$problem_id.cpp
+        targ=cf/$contest_id/$problem_id.cpp
+        mkdir -p cf/$contest_id/
+
+        if [ -f cf/$contest_id/$problem_id.cpp ]; then
+            echo File already exists
+        else
+            cp template.cpp cf/$contest_id/$problem_id.cpp
+        fi
+    else
+        echo Error: You must be in a folder named "Competitive-Programming" use this function
+    fi
+}
