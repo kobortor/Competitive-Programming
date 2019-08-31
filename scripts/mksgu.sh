b738a94acdccbc1110b2a334e4b3f4ff093d935e
3f9754c9ff87dc4cb66061b1582c56272e8ec83d <kobortor@gmail.com>
2019-08-31 14:38:24 -0400

    Added mksgu.sh

---
+++ scripts/mksgu.sh
@@ -0,0 +1,24 @@
+function mksgu () {
+    if (( $# != 1 )); then
+        echo We need exactly 1 argument!
+        return
+    fi
+
+    problem_id=$1
+    folder_id=$((problem_id / 100 * 100))
+
+    folder_name=$(basename $(pwd))
+    if [ "$folder_name" = "Competitive-Programming" ]; then
+        echo Setting targ=sgu/$folder_id/$problem_id.cpp
+        targ=sgu/$folder_id/$problem_id.cpp
+        mkdir -p sgu/$folder_name/
+
+        if [ -f sgu$folder_id/$problem_id.cpp ]; then
+            echo File already exists
+        else
+            cp template.cpp sgu/$folder_id/$problem_id.cpp
+        fi
+    else
+        echo Error: You must be in a folder named "Competitive-Programming" use this function
+    fi
+}
