1e4c2e70b2dd0a860c94fd8ac85e6dd4a371ac9d
3f9754c9ff87dc4cb66061b1582c56272e8ec83d <kobortor@gmail.com>
2019-09-11 01:30:27 -0400

    I am tired

---
+++ cf/0202/A.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define allof(x) (x).begin(), (x).end()
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+
+
+int main(){
+    cin.tie(0);
+    cin.sync_with_stdio(0);
+
+    string s;
+    cin >> s;
+
+    char mx = *max_element(allof(s));
+    cout << string(count(allof(s), mx), mx);
+}
---
+++ cf/0579/A.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define allof(x) (x).begin(), (x).end()
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+
+
+int main(){
+    cin.tie(0);
+    cin.sync_with_stdio(0);
+
+    int x;
+    cin >> x;
+
+    cout << bitset<32>(x).count();
+}
