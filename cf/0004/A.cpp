f6b123f8d0fbd3613fd6fb48865cb8459b9f86bf
kobortor <kobortor@gmail.com>
2020-05-24 23:23:00 -0400

    Added cf/0004/A

---
+++ cf/0004/A.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+
+// Created by kobortor On 23:06:18 on 24-05-2020
+
+using namespace std;
+
+#define allof(x) (x).begin(), (x).end()
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+
+
+int main(){
+    cin.tie(0);
+    cin.sync_with_stdio(0);
+
+    int w;
+    cin >> w;
+
+    if (w % 2 == 0 && w >= 4) {
+        cout << "YES\n";
+    } else {
+        cout << "NO\n";
+
+    }
+}
