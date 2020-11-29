13d0c8facc497103bb982f797a2d3189f88ff52a
Joey Yu <kobortor@gmail.com>
2020-11-29 02:12:27 -0500

    typo

--- scripts/mkcf.sh
+++ scripts/mkcf.sh
@@ -26,7 +26,7 @@ function mkcf () {
             echo File already exists
         else
             cf gen cpp > /dev/null
-            mv Competitive-Programming.cpp cf/$contest_id/$problem_id.cpp
+            mv template.cpp cf/$contest_id/$problem_id.cpp
         fi
 
         rm -r a.out .io_lock a.out.dSYM 2> /dev/null
